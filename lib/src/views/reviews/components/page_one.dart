3823673c34b50f2cfafe884ca591b3156fdaad45
muhammadhaad <haad1111997@gmail.com>
2024-01-02 06:44:35 +0500

    Refactor PageOne state to remove unnecessary comments and simplify code

--- lib/src/views/reviews/components/page_one.dart
+++ lib/src/views/reviews/components/page_one.dart
@@ -8,7 +8,7 @@ class PageOne extends StatefulWidget {
 }
 
 class _PageOneState extends State<PageOne> {
-  int _selectedIndex = -1; // Initialize with no item selected
+  int _selectedIndex = -1;
   List<String> text = ["Yes", "No"];
 
   @override
@@ -48,7 +48,7 @@ class _PageOneState extends State<PageOne> {
                   if (val == true) {
                     _selectedIndex = idx;
                   } else {
-                    _selectedIndex = -1; // None selected
+                    _selectedIndex = -1;
                   }
                 });
               },
@@ -59,61 +59,3 @@ class _PageOneState extends State<PageOne> {
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-
-// class PageOne extends StatefulWidget {
-//   const PageOne({super.key});
-
-//   @override
-//   State<PageOne> createState() => _PageOneState();
-// }
-
-// class _PageOneState extends State<PageOne> {
-//   bool _isChecked = true;
-
-//   List<String> text = ["Yes", "No"];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.center,
-//       children: [
-//         const SizedBox(
-//           width: 313,
-//           height: 36,
-//           child: Text(
-//             'Does the food tastes good?',
-//             style: TextStyle(
-//               color: Color(0xFF132513),
-//               fontSize: 24,
-//               fontFamily: 'Roboto',
-//               fontWeight: FontWeight.w500,
-//             ),
-//           ),
-//         ),
-//         Column(
-//           children: text
-//               .map((t) => CheckboxListTile(
-//                     title: Text(t,
-//                         style: const TextStyle(
-//                           color: Color(0xFF132513),
-//                           fontSize: 18,
-//                           fontFamily: 'Roboto',
-//                           fontWeight: FontWeight.w400,
-//                           height: 1.5,
-//                         )),
-//                     value: _isChecked,
-//                     onChanged: (val) {
-//                       setState(() {
-//                         _isChecked = val!;
-//                       });
-//                     },
-//                   ))
-//               .toList(),
-//         )
-//       ],
-//     );
-//   }
-// }
